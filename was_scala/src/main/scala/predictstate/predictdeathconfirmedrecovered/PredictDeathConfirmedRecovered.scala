59f1c1fe5be6fdc8bb4be1c8b9d76481de7cee72
Kapil Rajak <kapil@Tesseract-2.local>
2020-03-26 21:02:17 +0530

    predicting the spread

--- was_scala/src/main/scala/predictstate/predictdeathconfirmedrecovered/PredictDeathConfirmedRecovered.scala
+++ was_scala/src/main/scala/predictstate/predictdeathconfirmedrecovered/PredictDeathConfirmedRecovered.scala
@@ -1,17 +1,49 @@
 package predictstate.predictdeathconfirmedrecovered
 
 import predictstate.common.{InputDeathConfirmedRecoveredData, OutputDeathConfirmedRecoveredData, PredictTrait}
+import spray.json.DefaultJsonProtocol
 
+import scala.util.control.Breaks._
+import scala.collection.mutable.ListBuffer
 
+case class EveryDayPrediction(input:List[Long], r0: List[Double], minRateList: List[Long], minRate:Double, lastDateRateList: List[Long],
+                              lastDateRate:Double, maxRateList: List[Long], maxRate:Double, lastDate: String, province: String)
 object PredictDeathConfirmedRecovered {
-
-  def r0(confirmedCases: List[Int], i: Int, avgIncubationPeriod:Int): (Int, Int, Double) = {
+  val WorldPopulation = 7800000000l;
+  def r0(confirmedCases: List[Long], i: Int, avgIncubationPeriod:Int): (Long, Long, Double) = {
     (confirmedCases(i), confirmedCases(i+avgIncubationPeriod),
       (confirmedCases(i+avgIncubationPeriod)-confirmedCases(i))*1.0/confirmedCases(i))
   }
 
   //R0
-  def reproductionNumberR0(confirmedCases: List[Int], avgIncubationPeriod: Int): List[(Int,Int,Double)]  ={
+  def reproductionNumberR0(confirmedCases: List[Long], avgIncubationPeriod: Int): List[(Long,Long,Double)]  = {
     List.range(0, confirmedCases.size-avgIncubationPeriod).map(r0(confirmedCases, _, avgIncubationPeriod))
+  }
+
+  def extrapolation(confirmedCases: List[Long], avgIncubationPeriod: Int, numberOfDays: Int, lastDate:String, province:String):  EveryDayPrediction = {
+    val r0 = reproductionNumberR0(confirmedCases, avgIncubationPeriod)
+
+    val min = r0.minBy(_._3)._3
+    val max = r0.maxBy(_._3)._3
+    val nowRate = (confirmedCases.last -
+      confirmedCases(confirmedCases.size-1-avgIncubationPeriod)).toDouble/confirmedCases(confirmedCases.size-1-avgIncubationPeriod)
+    val minR= extrapolateEach(confirmedCases, numberOfDays, min)
+    val avgR = extrapolateEach(confirmedCases, numberOfDays, nowRate)
+    val maxR = extrapolateEach(confirmedCases, numberOfDays, max)
+    val r = EveryDayPrediction(confirmedCases, r0.map(_._3),minR, min, avgR,nowRate, maxR, max, lastDate, province)
+    r
+  }
+
+  def extrapolateEach(confirmedCases:List[Long], numberOfDays:Int, rate: Double): List[Long] ={
+    val listBuffer = new ListBuffer[Long]
+    breakable(
+      for(i <- 0 until numberOfDays)  {
+        val base = if(i<5) confirmedCases(confirmedCases.size-1-4+i) else listBuffer(i-4)
+        val updatedValue = base.toLong + (rate*base).round
+        listBuffer.append(updatedValue)
+        if(updatedValue>WorldPopulation) break()
+      }
+    )
+    listBuffer.toList
   }
 }
--- was_scala/src/test/scala/predictstate/PredictTraitTest.scala
+++ was_scala/src/test/scala/predictstate/PredictTraitTest.scala
@@ -1,27 +1,76 @@
 package predictstate
 
 import java.io.File
+import java.nio.file.Paths
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
 
 import com.github.tototoshi.csv.{CSVReader, CSVWriter}
 import org.scalatest.FunSuite
-import predictstate.predictdeathconfirmedrecovered.PredictDeathConfirmedRecovered
+import predictstate.predictdeathconfirmedrecovered.{EveryDayPrediction, PredictDeathConfirmedRecovered}
+import spray.json._
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val everyDayPredictionFormat = jsonFormat10(EveryDayPrediction)
+}
 
 class PredictTraitTest extends FunSuite {
+
   test("writing PredictDeathConfirmedRecovered output") {
-    val reader1 = CSVReader.open(new File("/Users/kapil/was/data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"))
-    val reader2 = CSVReader.open(new File("/Users/kapil/was/data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"))
+    val reader1 = CSVReader.open(new File("~/was/data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"))
+    val reader2 = CSVReader.open(new File("~/was/data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"))
 
     val r1 = reader1.all()
-    val result1 = r1.tail.map(row => (row(0), row(1), row.drop(4).map(_.toInt)))
+    val result1 = r1.tail.map(row => (row(0), row(1), row.drop(4).map(_.toLong)))
 
-    val rr1: List[Int] = List.range(0, result1.head._3.size).map(col => result1.map(_._3(col)).sum)
-
-    //val result2 = reader2.all().map(row => (row(0), row(1)))
-    //result1
-    val r0Result = PredictDeathConfirmedRecovered.reproductionNumberR0(rr1, 5);
-    //val toWrite = r0Result.map(e => (e._2, e._1._3))
+    val rr1: List[Long] = List.range(0, result1.head._3.size).map(col => result1.map(_._3(col)).sum)
+    val r0Result = PredictDeathConfirmedRecovered.reproductionNumberR0(rr1, 5)
     val writer = CSVWriter.open("/Users/kapil/was/derived_data/reproduction_number.csv")
     writer.writeAll(List(r1.head.drop(4), r0Result.map(_._3.toString)))
     writer.close()
+  }
+
+  def addDays(minDate: Date, i: Int): Date = {
+    val c = Calendar.getInstance()
+    c.setTime(minDate);
+    c.add(Calendar.DATE, i);
+    c.getTime
+  }
+
+  test("min avg max") {
+    val curDir:String = Paths.get(System.getProperty("user.dir")).getParent.toString
+    val confirmedGlobalReader = CSVReader.open(new File(curDir+"/data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"))
+
+    val confirmedGlobalFileContent = confirmedGlobalReader.all()
+    val minDateS = confirmedGlobalFileContent.head(4)
+    val maxDateS = confirmedGlobalFileContent.head.last
+
+    val dateFormat = new SimpleDateFormat("MM/dd/yy")
+    val dateFormatOutput = new SimpleDateFormat("yy_MM_dd")
+    val minDate = dateFormat.parse(minDateS)
+    val maxDate = dateFormat.parse(maxDateS)
+
+    var lastDate:Date = addDays(minDate, 6)
+    var dayDiff = 6;
+
+    while(!lastDate.after(maxDate)) {
+      val lastDateS = dateFormatOutput.format(lastDate)
+      val countryWithData = confirmedGlobalFileContent.tail.map(row => (row(0), row(1), row.slice(4, dayDiff+4).map(_.toLong)))
+
+      val dataTillNow: List[Long] = List.range(0, countryWithData.head._3.size).map(col => countryWithData.map(_._3(col)).sum)
+
+      val r = PredictDeathConfirmedRecovered.extrapolation(dataTillNow, 5, 1000, lastDateS,
+        "Global");
+      import MyJsonProtocol._
+      val rJson = r.toJson
+      val rString = rJson.prettyPrint
+
+      import java.io._
+      val pw = new PrintWriter(new File(curDir + "/derived_data/was_scala/predict_death_confirmed_recovered/" +
+        lastDateS.toString.replace("/", "_") + ".json"))
+      pw.write(rString)
+      pw.close
+      lastDate = addDays(lastDate, 1)
+      dayDiff +=1
+    }
   }
 }
